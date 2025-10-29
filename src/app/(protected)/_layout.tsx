ef405cc58c71cd6289486b84712d4a3c8e314be8
Reyvan Natechnoury <reyvannn@gmail.com>
2025-10-29 15:26:04 +0700

    Add delay when loading Clerk to prevent looping back to sign-in page

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -1,9 +1,31 @@
 // /(protected)/_layout.tsx
 import {useAuth} from "@clerk/clerk-expo";
 import {Redirect, Stack} from "expo-router";
+import {useEffect, useState} from "react";
+import {ActivityIndicator, View, StyleSheet} from "react-native";
 
 export default function AppLayout() {
-    const {isSignedIn} = useAuth();
+    const {isSignedIn, isLoaded} = useAuth();
+    const [waitedTooLong, setWaitedTooLong] = useState(false)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+                setWaitedTooLong(true);
+            }, // Callback after delay (3 seconds)
+            3000
+        );
+
+        return () => clearTimeout(timeout); // Cleanup function
+    }, [] // Empty dependency array means this effect runs once on mount
+    );
+
+    if (!isLoaded && !waitedTooLong) {
+        return (
+            <View style={styles.loadingContainer}>
+                <ActivityIndicator size="large"/>
+            </View>
+        )
+    }
 
     if (!isSignedIn) {
         return <Redirect href={'/sign-in'}/>
@@ -15,3 +37,11 @@ export default function AppLayout() {
         </Stack>
     )
 };
+
+const styles = StyleSheet.create({
+    loadingContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+})
